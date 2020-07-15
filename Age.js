c55eb7620490057edead328edee307b6734a4915
Balram531 <68235337+Balram531@users.noreply.github.com>
2020-07-15 06:56:47 -0700

    Add files via upload

---
+++ Age.js
@@ -0,0 +1,13 @@
+/*var age=prompt("Enter the age");
+
+if(age>=21)
+{
+	document.write("Can Drink");
+}
+else
+{
+	document.write("Cannot Drink");
+}
+*/
+var InputUser=prompt("This is a input user");
+console.log(InputUser);
